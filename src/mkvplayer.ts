c5de8d58e1f311ab0ddd65a97f64218d8751051b
Walker Aldridge <daldridge@lairdscomputer.com>
2025-03-10 16:09:04 -0500

    Add play endpoint to render page

--- src/mkvplayer.ts
+++ src/mkvplayer.ts
@@ -1,7 +1,7 @@
 // MKV to MP4 streamer with selectable audio track and subtitle track
 import express from 'express';
 import * as path from "node:path";
-import fs from 'fs';
+import fs from 'node:fs/promises';
 import { pipeline } from 'stream/promises';
 import { spawn } from 'node:child_process'
 import { v4 as uuid } from 'uuid';
@@ -80,10 +80,6 @@ mkvplayerAPI.get('/playvideo/:filename/:audioId', async (req, res) => {
             "pipe:1"
         ]);
 
-        ffmpeg.stderr.on("data", (data) => {
-            console.error("FFmpeg error:", data.toString());
-        });
-
         ffmpeg.on("exit", (code) => {
             if (code !== 0) console.error(`FFmpeg exited with code ${code}`);
         });
@@ -98,55 +94,9 @@ mkvplayerAPI.get('/playvideo/:filename/:audioId', async (req, res) => {
 
 mkvplayerAPI.get('/play/:filename/:audioId/:subtitleId', async (req, res) => {
     const filename = req.params.filename;
-    const audioId = parseInt(req.params.audioId, 10);
-    const subtitleId = parseInt(req.params.subtitleId, 10);
+    const audioId = req.params.audioId;
+    const subtitleId = req.params.subtitleId;
 
-    // Use ffmpeg to stream to a pipe with the correct audio and subs
-    const pipePath = "/tmp/video_" + uuid();
-    const mkfifo = spawn("mkfifo", [pipePath]);
-    await new Promise((resolve, reject) => {mkfifo.on("close",resolve)});
-
-    // Ensure the pipe exists
-    if (!fs.existsSync(pipePath)) {
-        console.error("Named pipe does not exist.");
-        res.status(500).send('Error streaming file - could not create fifo');
-    }
-    else {
-        try {
-            // Open readstream before write
-            const fileStream = fs.createReadStream(pipePath, { highWaterMark: 10240 * 1024 });
-
-            // Start FFmpeg writing to the named pipe
-            const ffmpeg = spawn("ffmpeg", [
-                "-y",
-                "-nostdin",
-                "-i", `${process.env.BASEPATH}/${filename}`,
-                "-map", "0:v:0",
-                "-map", `0:a:${audioId}`,
-                "-vf", `subtitles='${process.env.BASEPATH}/${filename}':si=${subtitleId}`,
-                "-movflags", "+faststart+frag_keyframe",
-                "-frag_duration", "8000000",
-                "-preset", "ultrafast",
-                "-f", "mp4",
-                pipePath
-            ], { stdio: ["ignore", "ignore", "pipe"] });
-
-            ffmpeg.stderr.on("data", (data) => {
-                console.error("FFmpeg error:", data.toString());
-            });
-
-            ffmpeg.on("exit", (code) => {
-                if (code !== 0) console.error(`FFmpeg exited with code ${code}`);
-            });
-
-            res.setHeader('Content-Type', 'video/mp4');
-            await pipeline(fileStream, res);
-        }
-        catch (error) {
-            console.error("Stream error:", error);
-        }
-        finally {
-            if (fs.existsSync(pipePath)) fs.unlinkSync(pipePath); // Cleanup
-        }
-    }
+    const page = await fs.readFile('src/videoPlayer.html', 'utf8');
+    res.send(page.replace('[filename]',filename).replace('[filename]',filename).replace('[subsId]',subtitleId).replace('[audioId]',audioId));
 })
